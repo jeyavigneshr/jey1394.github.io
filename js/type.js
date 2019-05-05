16382e3d433cca1d0407e1524c6c44786bf52f57
Jey <jeyavigneshr1394@gmail.com>
2019-05-05 22:59:20 +0530

    Initial Commit

---
+++ js/type.js
@@ -0,0 +1,48 @@
+var current = 0;
+var captionLength = 0;
+var captionList = [
+  'Hi',
+  'I love building stuffs',
+  'I enjoy playing PUBG',
+  'I like to play football',
+  'You guessed it Hala Madrid',
+  '***** REAL MADRID *****',
+]
+
+
+$(document).ready(function() {
+  setInterval('cursorBlink()', 600);
+  startTyping();
+});
+
+function startTyping() {
+  caption = captionList[current];
+  current = (current+1)%captionList.length;
+  type();
+}
+
+function type() {
+  $('#dynamic-desc').text('> '+caption.substr(0, captionLength++));
+  if (captionLength < caption.length + 1) {
+    setTimeout('type()',50);
+  } else {
+    setTimeout('erase()', 1000);
+  }
+}
+
+function erase() {
+  $('#dynamic-desc').text('> '+caption.substr(0, captionLength--));
+  if (captionLength >= 0) {
+    setTimeout('erase()', 50);
+  } else {
+    setTimeout('startTyping()',1000);
+  }
+}
+
+function cursorBlink() {
+      $('#cursor').animate({
+          opacity: 0
+      }, 'fast', 'swing').animate({
+          opacity: 1
+      }, 'fast', 'swing');
+}
